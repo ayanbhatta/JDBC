e2ee2d85d4674fba0e2751eaf36391f3c49f7bb0
ayanbhatta <ayanb8127@gmail.com>
2026-02-18 20:30:44 +0530

    statement interface and its methods

---
+++ JavaDatabaseConnectivity/src/org/jsp/jdbc/InresrRecord.java
@@ -0,0 +1,44 @@
+package org.jsp.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class InresrRecord {
+
+	public static void main(String[] args) {
+		Connection con = null;
+		Statement stmt = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307?user=root&password=Admin@123");
+			System.out.println("connection established");
+			stmt = con.createStatement();
+			stmt.executeUpdate("insert into btm.student values(1,'Tom',90);");
+			System.out.println("Record inserted");
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (stmt != null) {
+				try {
+					stmt.close();
+
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+
+				if (con != null) {
+					try {
+						con.close();
+
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+
+				}
+
+			}
+		}
+	}
+}
